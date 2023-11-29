eb5aa29361d7d8d47aa1b7b351faa57e2ce7ad7b
xpadev <xpadev@gmail.com>
2023-11-29 10:27:35 +0900

    fix: support niconicomments v0.2.62

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { IComment, Niwango } from "@/@types/types";
 import { CommentMapper } from "@/adapter";
+import { IRenderer } from "@/@types/niconicomments";
 
 let niwango: typeof Niwango;
 
 class NiconicommentsPluginNiwango {
   niwango: Niwango;
-  constructor(targetCanvas: HTMLCanvasElement, formattedComments: IComment[]) {
-    targetCanvas.width = 1920;
-    targetCanvas.height = 1080;
+  constructor(targetCanvas: IRenderer, formattedComments: IComment[]) {
     const comments = formattedComments.map(
       (comment) => new CommentMapper(comment),
     );
-    this.niwango = new niwango(targetCanvas, comments);
+    this.niwango = new niwango(targetCanvas.canvas, comments);
   }
   public draw(vpos: number) {
     this.niwango.draw(vpos, true);
